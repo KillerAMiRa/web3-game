1d73d67a7e96db196845fa49300b8805b6e7bf81
Lucas <1014930861@qq.com>
2025-12-29 15:01:12 +0800

    api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import { useConnect, useConnection, useConnectors, useDisconnect } from 'wagmi'
+import { Button } from 'antd'
+import { useEffect, useState } from 'react'
 
 function App() {
+  let ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+  let suits = ['♥️', '♦️', '♣️', '♠️']
+  let init = ranks.map((rank) => suits.map((suit) => ({ rank, suit }))).flat()
+  // console.log('🚀 - init:', init)
+
+  let [deck, setDeck] = useState<any>([])
+  useEffect(() => {
+    setDeck(init)
+  }, [])
+
   const connection = useConnection()
   const { connect, status, error } = useConnect()
   const connectors = useConnectors()
@@ -10,7 +22,25 @@ function App() {
 
   return (
     <>
+      <div className="p-2">
+        <div className="flex flex-wrap">
+          {deck.slice(0, 3).map((card: any, index: any) => (
+            <div key={index} className="w-12 h-20 border m-2 p-1">
+              <div className="">{card.rank} </div>
+              <div className="text-center">{card.suit} </div>
+              <div className="text-right">{card.rank} </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="p-2">2</div>
       <div>
+        <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button>
+      </div>
+      <hr />
+      <div className="p-2">
         <h2>Connection</h2>
 
         <div>
